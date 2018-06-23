252e6805e56bdf9dbeea2b60a67ab9c6f4b73269
danie <danie@DESKTOP-1M5N7PD>
2018-06-23 16:04:42 -0500

    FALTA LOGRAR CERRAR LAS VENTANAS ANTERIORES

--- ProyectoDEAdministraciondeEmpresa/src/controller/ProyectoListener.java
+++ ProyectoDEAdministraciondeEmpresa/src/controller/ProyectoListener.java
@@ -12,7 +12,14 @@ import java.awt.event.MouseWheelListener;
 
 import javax.swing.JOptionPane;
 
+import view.JFrameArticulosPrincipal;
+import view.JFrameComprasPrincipal;
+import view.JFrameEmpleadoPrincipal;
+import view.JFramePantallaPrincipal;
+import view.JFrameProveedoresPrincipal;
+import view.JFrameVentasPrincipal;
 import view.JpanelLeftPantallaPrincipal;
+import view.JpanelNortePantallaPrincipal;
 
 public class ProyectoListener implements ActionListener {
 	private static ProyectoListener proyectoListener = null;
@@ -31,6 +38,7 @@ public class ProyectoListener implements ActionListener {
 
 		this.jpanelStatusBar = jpanelStatusBar;
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		switch (e.getActionCommand().valueOf(e.getActionCommand())) {
@@ -59,38 +67,73 @@ public class ProyectoListener implements ActionListener {
 			break;
 		}
 	}
-	
-	
-	
 
 	private void boton1() {
 		System.out.println("Usted ha presionado el Boton 1");
+		JFramePantallaPrincipal jFramePantallaPrincipal = new JFramePantallaPrincipal();
+		jFramePantallaPrincipal.setBounds(0, 0, 600, 360);
+		jFramePantallaPrincipal.setVisible(true);
+		jFramePantallaPrincipal.setResizable(false);
+		jFramePantallaPrincipal.setLocationRelativeTo(null);
+
 	}
 
 	private void boton2() {
 		System.out.println("Usted ha presionado el Boton 2");
+		JFrameArticulosPrincipal jFrameArticulosPrincipal = new JFrameArticulosPrincipal();
+		jFrameArticulosPrincipal.setBounds(0, 0, 600, 360);
+		jFrameArticulosPrincipal.setVisible(true);
+		jFrameArticulosPrincipal.setResizable(false);
+		jFrameArticulosPrincipal.setLocationRelativeTo(null);
 	}
 
 	private void boton3() {
 		System.out.println("Usted ha presionado el Boton 3");
+		JFrameComprasPrincipal jFrameComprasPrincipal = new JFrameComprasPrincipal();
+		jFrameComprasPrincipal.setBounds(0, 0, 600, 360);
+		jFrameComprasPrincipal.setVisible(true);
+		jFrameComprasPrincipal.setResizable(false);
+		jFrameComprasPrincipal.setLocationRelativeTo(null);
 	}
 
 	private void boton4() {
 		System.out.println("Usted ha presionado el Boton 4");
+
+		JFrameVentasPrincipal jFrameVentasPrincipal = new JFrameVentasPrincipal();
+		jFrameVentasPrincipal.setBounds(0, 0, 600, 360);
+		jFrameVentasPrincipal.setVisible(true);
+		jFrameVentasPrincipal.setResizable(false);
+		jFrameVentasPrincipal.setLocationRelativeTo(null);
 	}
 
 	private void boton5() {
 		System.out.println("Usted ha presionado el Boton 5");
+		JFrameProveedoresPrincipal jFrameProveedoresPrincipal = new JFrameProveedoresPrincipal();
+		jFrameProveedoresPrincipal.setBounds(0, 0, 600, 360);
+		jFrameProveedoresPrincipal.setVisible(true);
+		jFrameProveedoresPrincipal.setResizable(false);
+		jFrameProveedoresPrincipal.setLocationRelativeTo(null);
 	}
 
 	private void boton6() {
 		System.out.println("Usted ha presionado el Boton 6");
-	}
-	
-	private void boton7() {
-		System.out.println("Usted ha presionado el Boton 7");
-		
+
+		JFrameEmpleadoPrincipal jFrameEmpleadoPrincipal = new JFrameEmpleadoPrincipal();
+		jFrameEmpleadoPrincipal.setBounds(0, 0, 600, 360);
+		jFrameEmpleadoPrincipal.setVisible(true);
+		jFrameEmpleadoPrincipal.setResizable(false);
+		jFrameEmpleadoPrincipal.setLocationRelativeTo(null);
 	}
 
+	private void boton7() {
+		System.out.println("Usted ha presionado el Boton 7");
+
+		JFrameEmpleadoPrincipal jFrameEmpleadoPrincipal = new JFrameEmpleadoPrincipal();
+		jFrameEmpleadoPrincipal.setBounds(0, 0, 600, 360);
+		jFrameEmpleadoPrincipal.setVisible(true);
+		jFrameEmpleadoPrincipal.setResizable(false);
+		jFrameEmpleadoPrincipal.setLocationRelativeTo(null);
+
+	}
 
 }
