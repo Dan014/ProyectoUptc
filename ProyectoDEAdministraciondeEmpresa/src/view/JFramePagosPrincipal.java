b727f9413794de85d7226025c8a9c125121b3536
danie <danie@DESKTOP-1M5N7PD>
2018-06-25 12:50:26 -0500

    Agrugue la vista De Pagos

---
+++ ProyectoDEAdministraciondeEmpresa/src/view/JFramePagosPrincipal.java
@@ -0,0 +1,48 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+
+
+
+public class JFramePagosPrincipal extends JFrame {
+	
+	private JpanelNortePantallaPrincipal jpanelNorte;
+	private JpanelCenterPagosPrincipal jpanelCenter;
+	private JpanelLeftPantallaPrincipal jpanelLeft;
+	private JMeuBarPantallaPrincipal jMeuBarPantallaPrincipal;
+
+	
+	public JFramePagosPrincipal() {
+		super(Constant.APP_NAME);
+		this.jpanelNorte =  new JpanelNortePantallaPrincipal();
+		this.jpanelCenter = new JpanelCenterPagosPrincipal();
+		this.jpanelLeft =  new JpanelLeftPantallaPrincipal();
+		this.jMeuBarPantallaPrincipal =  new JMeuBarPantallaPrincipal();
+
+		init();
+
+	}
+
+	private void init() {
+
+		this.setResizable(true);
+		this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+		this.setJMenuBar(jMeuBarPantallaPrincipal);
+
+		this.add(jpanelNorte, BorderLayout.NORTH);
+		this.add(jpanelCenter, BorderLayout.CENTER);
+		this.add(jpanelLeft, BorderLayout.WEST);
+
+
+		
+		
+		
+		this.setVisible(true);
+	}
+
+}
---
+++ ProyectoDEAdministraciondeEmpresa/src/view/TestPagoPrincipal.java
@@ -0,0 +1,8 @@
+package view;
+
+
+public class TestPagoPrincipal {
+public static void main(String[] args) {
+	JFramePagosPrincipal jFramePagosPrincipal = new JFramePagosPrincipal();
+}
+}
